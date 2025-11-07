24b889060d48ff0136f9d614889842d45bd0076f
Nishchay <nishchay.vns2000@gmail.com>
2025-11-07 22:49:39 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (28.86%) - LeetSync

---
+++ 979-di-string-match/di-string-match.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        vector<int> res;
+        int n = s.size(), j = 0;
+        for (int i = 0; i <= s.size(); ++i) {
+            res.push_back(i);
+            if (i == n || s[i] == 'I') {
+                // reverse the segment from j to i (inclusive)
+                reverse(res.begin() + j, res.end());
+                j = i + 1;
+            }
+        }
+        return res;
+    }
+};
