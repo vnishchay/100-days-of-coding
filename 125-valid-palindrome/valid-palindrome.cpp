9970a85f113fcf34537ccf3dd099f7af9cd64dd8
Nishchay <nishchay.vns2000@gmail.com>
2025-11-08 22:46:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (99.45%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            // Skip non-alphanumeric characters
+            while (l < r && !isalnum(s[l])) l++;
+            while (l < r && !isalnum(s[r])) r--;
+            
+            // Compare after converting both to lowercase
+            if (tolower(s[l]) != tolower(s[r])) return false;
+
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
