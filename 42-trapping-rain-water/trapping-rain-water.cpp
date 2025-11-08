76ea404f7f39baf9d5201d6df125ea6964a123ab
Nishchay <nishchay.vns2000@gmail.com>
2025-11-08 19:55:19 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (93.32%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    //total water is trapped into the bars
     int trap(vector<int>& h) {
-        int l=0,r=h.size()-1,lmax=INT_MIN,rmax=INT_MIN,ans=0;
-        while(l<r){
-            lmax=max(lmax,h[l]);
-            rmax=max(rmax,h[r]);
-            ans+=(lmax<rmax)?lmax-h[l++]:rmax-h[r--];
+        int l = 0 ; int r=  h.size()-1;  int lmax = INT_MIN; int rmax = INT_MIN; 
+        int total = 0; 
+        while(l < r) {
+             lmax = max(h[l], lmax); 
+             rmax = max(h[r], rmax); 
+             total += (lmax < rmax) ? lmax - h[l++] : rmax - h[r--]; 
         }
-        return ans;
+        return total ; 
     }
 };
